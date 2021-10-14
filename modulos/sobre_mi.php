de723307fc701f1a865f1052f3ed889ad2b67089
Oscar Bautista <oscar04262000@gmail.com>
2021-10-14 00:42:03 -0500

    index.php

---
+++ modulos/sobre_mi.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacerca de mi</title>
+    <link rel="icon" href="icon/usuario.png">
+    <link rel="stylesheet" href="css/styleSobremi.css">
+    <link rel="stylesheet"href="css/style.css">
+</head>
+<body>
+    <section>
+            <!--Colocar mi fotografia-->
+            <div>
+                <img src="icon/avatar.png" 
+                atl="Oscar Bautista Gaytan - Fotografia"
+                width="20%"
+                height="20%">
+                <h2 id="div__h2"> Presentación</h2>
+                <p><b>Nombre: </b>Oscar Bautista Gaytan</p>
+                <p><b>Edad: </b>20 años</p>
+                <p><b>Sexo:</b> Masculino</p>
+                <p><b>Fecha de nacimiento:</b>  26 de abril del 2000</p>
+                <!--Lugar de nacimiento-->
+                <p><b>Nacionalidad:</b> Mexicana</p>
+                <a href="mailto:oscar04262000@gmail.com?subject=Contacto&body=Escribir">Enviar email</a>
+                <p>Estudiante en Ingenieria en Desarrollo de Software</p>
+            </div>
+    </section>
+</body>
+</html>
